ba31e3f111b1ef472fcea568437d3e8c3eb49abd
wxj10 <2387861419@qq.com>
2020-08-29 22:20:05 +0800

    text

--- pages/elevatorMonitor/elevatorMonitor.vue
+++ pages/elevatorMonitor/elevatorMonitor.vue
@@ -3,19 +3,19 @@
 		<!-- nav -->
 		<view class="ele-nav">
 			<view class="ele-nav-li bg1">
-				<view class="ele-nav-li-num">50</view>
+				<view class="ele-nav-li-num">{{ classList.line+classList.online || 0 }}</view>
 				<view class="ele-nav-li-text">电梯总数</view>
 			</view>
 			<view class="ele-nav-li bg2">
-				<view class="ele-nav-li-num">31</view>
+				<view class="ele-nav-li-num">{{ classList.line || 0 }}</view>
 				<view class="ele-nav-li-text">在线电梯</view>
 			</view>
 			<view class="ele-nav-li bg3">
-				<view class="ele-nav-li-num">20</view>
+				<view class="ele-nav-li-num">{{ classList.fault || 0 }}</view>
 				<view class="ele-nav-li-text">故障数量</view>
 			</view>
 			<view class="ele-nav-li bg4">
-				<view class="ele-nav-li-num">20</view>
+				<view class="ele-nav-li-num">{{ classList.maint || 0 }}</view>
 				<view class="ele-nav-li-text">检修数量</view>
 			</view>
 		</view>
@@ -23,10 +23,10 @@
 		<view class="ele-bot">
 			<view class="ele-bot-top">
 				<view class="ele-bot-top-xl">
-					<view class="ele-bot-top-xl-ad1">
-						重庆<image src="../../static/image/wxj/xia.png" mode=""></image>
+					<view class="ele-bot-top-xl-ad1" @click="changeAddres">
+						{{ addText }}<image src="../../static/image/wxj/xia.png" mode=""></image>
 					</view>
-					<view class="ele-bot-top-xl-ad2">
+					<view class="ele-bot-top-xl-ad2" @click="changeXq">
 						全部地区<image src="../../static/image/wxj/xia.png" mode=""></image>
 					</view>
 				</view>
@@ -67,20 +67,58 @@
 				</view>
 			</view>
 		</view>
+		<van-popup
+		  :show="addresShow"
+		  position="bottom"
+		  @close="onClose"
+		>
+		<van-area @confirm="isokAdd" @cancel="onClose" :area-list="areaList" />
+		</van-popup>
+		<van-popup
+		  :show="xqShow"
+		  position="bottom"
+		  @close="onCloseXq"
+		>
+			<scroll-view scroll-y="true" style="height: 700rpx;">
+				<view class="proTitle">请选择小区</view>
+				<view class="xq-list">
+					<view class="xq-list-li" v-for="(item,index) in xqList" :key="item.index" @click="xqItem(item.id)">
+						<view class="xq-list-li-text" :class="item.id == xqCode?'xq-list-li-text_act':''">{{ item.name }}</view>
+						<view class="xq-list-li-gx" v-if="item.id == xqCode">
+							<van-icon name="success" color="#1989fa" size="18px"/>
+						</view>
+					</view>
+				</view>
+			</scroll-view>
+		</van-popup>
 	</view>
 </template>
 
 <script>
+	import request from '@/service/request.js'
 	import Null from '@/components/uni-null/uni-null.vue'
 	import uniLoadMore from "@/components/uni-load-more/uni-load-more.vue"
+	import AREA from "@/utils/area.js"
 	export default {
 		components:{
 			Null,uniLoadMore
 		},
 		data() {
 			return {
+				xqShow: false,
+				xqText: '全部地区',
+				xqCode: 0,
+				addText: '九龙坡区',
+				dqCode: 500107,
+				addresShow: false,
+				classList:{},
 				status: 'more',
 				title: '暂无数据',
+				areaList: {},
+				xqList:[
+					{id:1,name:'学林佳苑A栋'},
+					{id:1,name:'学林佳苑A栋'}
+				],
 				list:[
 					{id:1,studes:1,name:'奔力绿色空间1号楼1号 客梯', number:'230000007 【 23000000723000000711】',addrss:'重庆市九龙坡区渝州路歇台子党校科技大楼9楼'},
 					{id:2,studes:2,name:'奔力绿色空间1号楼1号 客梯', number:'230000007 【 23000000723000000711】',addrss:'重庆市九龙坡区渝州路歇台子党校科技大楼9楼'},
@@ -94,8 +132,89 @@
 				}
 			}
 		},
+		onLoad() {
+			let that = this;
+			that.areaList = AREA;
+			that.getClassNx();
+			that.getList();
+			that.getXqText();
+			that.getUserInfo();
+		},
 		methods: {
-			
+			xqItem(id){
+				let that = this;
+				that.xqCode = id;
+			},
+			isokAdd(e){
+				console.log(e)
+				let that = this;
+				let value = e.target.values;
+				let addText = value[2].name; //value[0].name+'/'+value[1].name+'/'+
+				let dqCode = value[2].code;
+				that.addText = addText;
+				that.dqCode = dqCode;
+				that.xqText= '全部地区',
+				that.xqCode= 0,
+				that.onClose();
+			},
+			onCloseXq(){
+				let that = this;
+				that.xqShow = false;
+			},
+			changeXq(){
+				let that = this;
+				that.xqShow = true;
+			},
+			changeAddres(){
+				let that = this;
+				that.addresShow = true;
+			},
+			onClose(){
+				let that = this;
+				that.addresShow = false;
+			},
+			//获取电梯各类型数量
+			getClassNx(){
+				let that = this;
+				let data = {};
+				request.post('/statistical/elevator',data).then((res)=>{
+					if(res.code == 1){
+						that.classList = res.data
+					}
+				})
+			},
+			//获取小区
+			getXqText(){
+				let that = this;
+				let data = {
+					area_id: that.dqCode
+				};
+				request.post('/region/village',data).then((res) =>{
+					console.log(res)
+				})
+			},
+			//获取电梯列表
+			getList(){
+				let that = this;
+				let data = {
+					province_id:500000,
+					limit: 10,
+					page: 1,
+					village_id: 2
+				};
+				request.post('/lift/monitoring',data).then((res) => {
+					console.log(res)
+				})
+			},
+			getUserInfo(){
+				let that = this;
+				let data = {
+					refresh: 1
+				};
+				request.post('/user_info',data).then((res) => {
+					console.log(res)
+				})
+			}
 		},
 		onPullDownRefresh(){
 			console.log('下拉开始')
@@ -118,6 +237,26 @@
 </script>
 
 <style scoped>
+	.proTitle{
+		font-size: 28rpx;
+		text-align: center;
+		padding: 40rpx;
+		box-sizing: border-box;
+		font-weight: bolder;
+		color: #333;
+	}
+	.xq-list-li{
+		padding: 20rpx 30rpx;
+		box-sizing: border-box;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 28rpx;
+		border-bottom: 2rpx solid #eee;
+	}
+	.xq-list-li-text_act{
+		color: #1989fa !important;
+	}
 	.ele-nav{
 			padding: 30rpx;
 			box-sizing: border-box;
